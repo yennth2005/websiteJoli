56d140a4e9e4a18076a57182d19008c1ee14dcee
vuongson <sonvhph50708@gmail.com>
2025-04-07 00:45:24 +0700

    test color

---
+++ app/Models/ProductVariant.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ProductVariant extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['product_id', 'color_id', 'price', 'sale_price', 'stock'];
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+
+    //color
+    public function color()
+    {
+        return $this->belongsTo(Color::class);
+    }
+}
